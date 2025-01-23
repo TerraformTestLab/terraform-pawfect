19734d443c12bdb821a0c1e96e40cdc3db608bc7
Sujay Samanta <sujay.samanta@hashicorp.com>
2025-01-23 21:18:02 +0530

    Added Terraform config

---
+++ maint.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+  }
+}
+
+provider "random" {}
+
+# Generate a random pet name with a unique suffix
+resource "random_pet" "unique_identifier" {
+  length    = 2  # Generates a name with 2 words
+  separator = "-"
+  keepers = {
+    # This ensures a new pet name on each apply
+    timestamp = timestamp()
+  }
+}
+
+# Generate an additional random pet as a backup identifier
+resource "random_pet" "backup_identifier" {
+  length    = 3
+  separator = "_"
+}
+
+output "primary_identifier" {
+  value       = random_pet.unique_identifier.id
+  description = "A unique identifier generated using a random pet name"
+}
+
+output "backup_identifier" {
+  value       = random_pet.backup_identifier.id
+  description = "An alternative unique identifier using a random pet name"
+}
+
+output "combined_identifier" {
+  value       = "${random_pet.unique_identifier.id}-${random_pet.backup_identifier.id}"
+  description = "A combined unique identifier using two random pet names"
+}
